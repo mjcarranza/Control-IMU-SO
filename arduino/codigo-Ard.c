e1bd7e0921a18a24e64bc85f1f66462ce020b3d7
Mario <48070830+mjcarranza@users.noreply.github.com>
2024-06-05 14:49:45 -0600

    add client and server(cluster)

--- arduino/codigo-Ard.c
+++ arduino/codigo-Ard.c
@@ -1,61 +1,57 @@
 #include <stdio.h>
 #include <mpi.h>
-#include <openssl/evp.h> // Biblioteca para cifrado OpenSSL
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <termios.h>
 #include <string.h>
-#include <errno.h>
-#include <string.h>
-#include "driverLib.h" // se incluye la libreria hacia el driver
-#include "encriptLib.h" // se incluye libreria para realizar la encriptacion de los datos
-
+#include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 #define MAX_SAMPLES 500
 #define MOVING_AVERAGE_WINDOW 10
 
-// Función para configurar la conexión serial
-int configure_serial(int fd) {
-    struct termios tty;
-    memset(&tty, 0, sizeof tty);
+#define PORT 8080
+#define BUFFER_SIZE 2048
 
-    if (tcgetattr(fd, &tty) != 0) {
-        printf("Error %d from tcgetattr: %s\n", errno, strerror(errno));
-        return -1;
-    }
-
-    cfsetospeed(&tty, B9600);
-    cfsetispeed(&tty, B9600);
-
-    tty.c_cflag |= (CLOCAL | CREAD);    // habilita el receptor y establece localmente
-    tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;         // 8 bits de datos
-    tty.c_cflag &= ~PARENB;     // sin paridad
-    tty.c_cflag &= ~CSTOPB;     // 1 bit de stop
-    tty.c_cflag &= ~CRTSCTS;    // sin control de flujo
-
-    tty.c_lflag &= ~ICANON;
-    tty.c_lflag &= ~ECHO;       // sin eco
-    tty.c_lflag &= ~ECHOE;      // sin eco de borrado
-    tty.c_lflag &= ~ISIG;       // sin interpretación de caracteres INTR, QUIT, SUSP
-
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // sin control de flujo de software
-    tty.c_iflag &= ~(ICRNL | INLCR);        // sin mapeo de CR a NL y viceversa
-
-    tty.c_oflag &= ~OPOST; // sin procesamiento de salida
-
-    // Establece el tiempo de espera para la lectura
-    tty.c_cc[VMIN] = 1;
-    tty.c_cc[VTIME] = 10;
-
-    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
-        printf("Error %d from tcsetattr: %s\n", errno, strerror(errno));
-        return -1;
-    }
-
-    return 0;
+void initialize_openssl() {
+    SSL_load_error_strings();
+    OpenSSL_add_ssl_algorithms();
 }
+
+void cleanup_openssl() {
+    EVP_cleanup();
+}
+
+SSL_CTX* create_context() {
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = SSLv23_server_method();
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+void configure_context(SSL_CTX *ctx) {
+    SSL_CTX_set_ecdh_auto(ctx, 1);
+
+    /* Set the key and cert */
+    if (SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM) <= 0 ) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+}
+
 
 // Función para calcular la media de los valores
 double calculate_average(int *values, int count) {
@@ -87,147 +83,203 @@ int calculate_moving_average_and_compare(int *values, int sample_count, const ch
 
 
 int main() {
-    unsigned char ciphertext[128]; // Buffer para recibir el texto cifrado
-    unsigned char decryptedtext[128]; // Buffer para almacenar el texto descifrado
-    int ciphertext_len;
+
+    // variables para la inicializacion de OpenMPI
     MPI_Status status;
-    
     MPI_Init(&argc, &argv); // Inicializar el entorno MPI
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Obtener el rango del proceso actual
     MPI_Comm_size(MPI_COMM_WORLD, &size); // Obtener el tamaño del comunicador (número de procesos)
-    // Clave y IV para cifrado (esto es solo un ejemplo, en producción debes gestionarlos de manera segura)
-    unsigned char *key = (unsigned char *)"01234567890123456789012345678901"; // Clave de 256 bits
-    unsigned char *iv = (unsigned char *)"0123456789012345"; // IV de 128 bits
+    
 
-    int fd;
-    struct termios options;
-    //char *portname = "/dev/ArduinoDriver3"; // Set Arduino port
-    const char *portname = "/dev/ttyACM1";
-    char command[20];
-    char inputIzq[5];
-    char inputDer[5];
+    // variables para la lectura y descomposicion de los datos del IMU 
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+    int sample_count = 500;
+    int contM = 0;
+    int contS = 0;
 
-    // Abre el puerto serie
-    fd = open(portname, O_RDWR);
-    if (fd == -1) {
-        perror("open_port: Unable to open /dev/ArduinoDriver3 - ");
-        return -1;
+    initialize_openssl();
+    SSL_CTX *ctx = create_context();
+    configure_context(ctx);
+    int gx_values[MAX_SAMPLES];
+    int gy_values[MAX_SAMPLES];
+
+        initialize_openssl();
+    SSL_CTX *ctx = create_context();
+    configure_context(ctx);
+
+    // Crear el socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
 
-    // Configura el puerto serie
-    tcgetattr(fd, &options);
-    cfsetispeed(&options, B9600);
-    cfsetospeed(&options, B9600);
-    options.c_cflag |= (CLOCAL | CREAD);
-    tcsetattr(fd, TCSANOW, &options);
+    // Asignar dirección y puerto
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-    // Asegurar que la conexión serie esté en modo bloqueante
-    fcntl(fd, F_SETFL, 0);
+    // Enlazar el socket
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Escuchar las conexiones entrantes
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Aceptar una conexión
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    SSL *ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, new_socket);
+
 
     // Ejecutar todo el proceso dentro de este while
     while(1){
-        if(rank == 0){  // si estamos en el nodo master
-            // en esta parte se tiene que recibir los datos del arduino y procesarlos para enviarlos
-            char message[] = "-1351,1068"; // Mensaje a cifrar y enviar
-            unsigned char ciphertext[20]; // Buffer para almacenar el texto cifrado
-            int ciphertext_len;
+        MPI_Status status;
+        // recibir los datos desde el cliente
+        if (SSL_accept(ssl) <= 0) {
+            ERR_print_errors_fp(stderr);
+        } else {
+            // Recibir datos del cliente
+            int valread = SSL_read(ssl, buffer, BUFFER_SIZE);
+            if (valread > 0) {
+                // los valores se guardan en gx y gy
+                memcpy(gx_values, buffer, sample_count * sizeof(int));
+                memcpy(gy_values, buffer + sample_count * sizeof(int), sample_count * sizeof(int));
 
-            // Cifrar el mensaje
-            ciphertext_len = encrypt((unsigned char*)message, strlen(message), key, iv, ciphertext);
-
-            // Enviar el mensaje cifrado a todos los demás procesos
-            for (int i = 1; i < size; i++) {
-                MPI_Send(ciphertext, ciphertext_len, MPI_UNSIGNED_CHAR, i, 0, MPI_COMM_WORLD);
-                printf("Mensaje enviado al nodo 1");
+            } else {
+                printf("No data received\n");
             }
+        }
 
-            // Recibir el mensaje cifrado del Nodo 1 con las coordenadas para los servos
-            MPI_Recv(ciphertext, 128, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, &status);
-            MPI_Get_count(&status, MPI_UNSIGNED_CHAR, &ciphertext_len);
+        
+        // si estamos en el nodo master, se ejecuta el analisis estadistico para dx
+        if(rank == 0){  
+            // Prepara un buffer para enviar el arreglo dy
+            int gy_size = 500; // Tamaño del arreglo gy
+            int dx;
+            int *gy_buffer = malloc(gy_size * sizeof(int)); 
+            memcpy(gy_buffer, gy_values, gy_size * sizeof(int)); // Copia los datos de gy a gy_buffer
+            MPI_Send(gy_buffer, gy_size, MPI_INT, 0, 0, MPI_COMM_WORLD);// Enviar el arreglo dy al proceso slave
+            free(gy_buffer);// Libera la memoria del buffer
 
-            // Descifrar el mensaje
-            decrypt(ciphertext, ciphertext_len, key, iv, decryptedtext);
+            // hacer el procesamiento estadistico para dx
+            while (contM < MAX_SAMPLES) {
+                if (contM >= MOVING_AVERAGE_WINDOW + 1) {
+                    dx = calculate_moving_average_and_compare(gx_values, contM, "GX");
+                    // recibir los datos del nodo slave
+                    MPI_Status status;
+                    int gy_size;
+                    int dy;
+                    MPI_Recv(&gy_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status); // Recibe el tamaño del arreglo dy enviado por el nodo maestro
+                    int *gy = malloc(gy_size * sizeof(int)); // Aloja memoria para almacenar el arreglo dy
+                    MPI_Recv(gy, gy_size, MPI_INT, 0, 0, MPI_COMM_WORLD, &status); // Recibe el arreglo dy
+                    // pasar los datos a cadena ed caracteres
+                    char cadx[20], cady[20];
+                    sprintf(cadx, "%d", dx); // el resultado esta en cadena
+                    sprintf(cady, "%d", dy); // el resultado esta en cadena
 
-            // Mostrar el mensaje que llega desde el nodo 1
-            printf("El nodo %d envio el mensaje: %s\n", rank, decryptedtext);
+                    // empaquetar resulatdos 
+                    char resultado[100]; // Tamaño suficientemente grande para contener ambas cadenas y la coma
+                    strcpy(resultado, cadx);
+                    strcat(resultado, ",");
+                    strcat(resultado, cady);
+                    // encriptar los datos
+                    
+                    // enviar los datos encriptados
+                    if (SSL_connect(ssl) <= 0) {
+                        ERR_print_errors_fp(stderr);
+                    } else {
+                        // Enviar el buffer al servidor
+                        SSL_write(ssl, resultado, 2 * sample_count * sizeof(int));
+                        printf("Data sent\n");
+                    }
+                    printf("Data sent to client\n");
+                }
+                cont ++;
+            }
+            
+            
 
-            // enviar las posiciones a los servos
-        }else{
+
+            
+        }else{ // si estamos en el nodo slave, recibir, ejecutar y enviar 
             MPI_Status status;
-            MPI_Recv(ciphertext, 128, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, &status);
-            int ciphertext_len;
-            MPI_Get_count(&status, MPI_UNSIGNED_CHAR, &ciphertext_len);
-
-            decrypt(ciphertext, ciphertext_len, key, iv, decryptedtext); // el texto decifrado se guarda en decryptedtext
-
-            // en esta parte se hace el analisis estadistico de los datos 
-            // y se calculan las posiciones de los servos
-
-            // encriptar los datos a enviar (estan en la variable posiciones)
-            ciphertext_len = encrypt((unsigned char*)posiciones, strlen(posiciones), key, iv, ciphertextSend);
-
-            // enviar de vuelta los datos al nodo 1
-            MPI_Send(&ciphertextSend, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            int gy_size;
+            int dy;
+            MPI_Recv(&gy_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status); // Recibe el tamaño del arreglo dy enviado por el nodo maestro
+            int *gy_buffer = malloc(gy_size * sizeof(int)); // Aloja memoria para almacenar el arreglo dy
+            MPI_Recv(gy_buffer, gy_size, MPI_INT, 0, 0, MPI_COMM_WORLD, &status); // Recibe el arreglo dy
+            // Procesa los datos recibidos
+            // hacer el procesamiento estadistico para dx
+            while (contS < MAX_SAMPLES) {
+                if (contS >= MOVING_AVERAGE_WINDOW + 1) {
+                    dy = calculate_moving_average_and_compare(gy_values, contS, "GY");
+                    // Prepara un buffer para enviar el  dy
+                    int dy_size = 10; // Tamaño del  gy
+                    int *dy_buffer = malloc(dy_size * sizeof(int)); 
+                    memcpy(dy_buffer, dy, dy_size * sizeof(int)); // Copia los datos de gy a gy_buffer
+                    MPI_Send(dy_buffer, dy_size, MPI_INT, 0, 0, MPI_COMM_WORLD);// Enviar el arreglo dy al proceso slave
+                    free(dy_buffer);// Libera la memoria del buffer
+                }
+                cont ++;
+            }
+            
+            free(gy_buffer);
             
         }
     }
-    
 
-    //entrada del servo
-    /*while (1) {
-        // Leer la entrada del usuario para el servo Izq
-        printf("Introduce la posición para el servo Izq:\n");
-        fgets(inputIzq, sizeof(inputIzq), stdin);
-        inputIzq[strcspn(inputIzq, "\n")] = 0; // Eliminar el carácter de nueva línea
+    // finalizacion del programa
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    close(new_socket);
+    close(server_fd);
+    SSL_CTX_free(ctx);
+    cleanup_openssl();
+    MPI_Finalize(); // Finalizar el entorno MPI
+    return 0;
+}
 
-        // Comparar la entrada con "exit"
-        if (strcmp(inputIzq, "exit") == 0) {
-            break;
-        }
+                    // pasar los datos a cadena ed caracteres
+                    // empaquetar resulatdos 
+                    // encriptar los datos
+                    // enviar los datos encriptados
 
-        // Leer la entrada del usuario para el servo Der
-        printf("Introduce la posición para el servo Der:\n");
-        fgets(inputDer, sizeof(inputDer), stdin);
-        inputDer[strcspn(inputDer, "\n")] = 0; // Eliminar el carácter de nueva línea
 
-        // Comparar la entrada con "exit"
-        if (strcmp(inputDer, "exit") == 0) {
-            break;
-        }
 
-        // Enviar comando al Arduino para ambos servos
-        snprintf(command, sizeof(command), "%s,%s\n", inputIzq, inputDer);
-        write(fd, command, strlen(command));
-    }*/
-    
-    int gx_values[MAX_SAMPLES];
-    int gy_values[MAX_SAMPLES];
-    int gz_values[MAX_SAMPLES];
-    int sample_count = 0;
 
-    char read_buffer[256];
-    memset(read_buffer, 0, sizeof(read_buffer));
 
-    const int arduino_interval_ms = 100;  // Intervalo de envío del Arduino en ms
-    const int usleep_interval = (arduino_interval_ms - 10) * 1000;  // Usleep en microsegundos, ajustado
+/* TAREAS PARA HOY
 
-    while (sample_count < MAX_SAMPLES) {
-        int n_read = receive_data(fd, read_buffer, sizeof(read_buffer) - 1);
-        if (n_read > 0) {
-            read_buffer[n_read] = '\0'; // Asegura que la cadena esté terminada en NULL
-            printf("Read: %s\n", read_buffer);
+- Meter lo del arduino en la parte de ejecucion distribuida
 
-            // Extraer valores gx, gy y gz de la cadena
-            int gx, gy,dx,dy;
-            if (sscanf(read_buffer, "GX:%d GY:%d ", &gx, &gy) == 2) {
-                gx_values[sample_count] = gx;
-                gy_values[sample_count] = gy;
-                
+- Hacer que el arduino reciba la cadena de posiciones para los servos correctamente
 
-                sample_count++;
+- Hacer que este programa termine de manera "elegante"
 
-                // Calcular y comparar las medias móviles cuando hay suficientes muestras
+- hacer lo del envio de datos entre nodos por medio de cifrado
+
+- Verificar que los datos que se leen luego de desencriptarlos sean los correctos en ambos nodos
+
+*/
+
+/*
+// Calcular y comparar las medias móviles cuando hay suficientes muestras
                 if (sample_count >= MOVING_AVERAGE_WINDOW + 1) {
                     dx=calculate_moving_average_and_compare(gx_values, sample_count, "GX");
                     dy=calculate_moving_average_and_compare(gy_values, sample_count, "GY");
@@ -240,44 +292,58 @@ int main() {
                     char send_data_str[] = "Hello Arduino\n";
 
 
-                    /*
-                    char str1[12];  
-                    char str2[12];
-                    
-                    // Convertimos los enteros a cadenas
-                    snprintf(str1, sizeof(str1), "%d", dx);
-                    snprintf(str2, sizeof(str2), "%d", dy);
-                    
-                    // Calculamos el tamaño total necesario para la cadena resultante
-                    int totalLength = strlen("dx") +strlen(str1) + strlen("dy") + strlen(str2) + strlen(send_data_str) + 1; // +1 para el carácter nulo
-                    
-                    // Definimos el array resultante
-                    char result[totalLength];
-                    
-                    // Inicializamos la cadena resultante con la primera cadena
-                    strcpy(result, str1);
-                    
-                    // Concatenamos la segunda cadena
-                    strcat(result, str2);
-                    
-                    // Concatenamos el texto adicional
-                    strcat(result, send_data_str);*/
-
                     if (send_data(fd, send_data_str) < 0) {
                         close(fd);
                         return -1;
                     }
                     usleep(100000);
                 }
-            }
-        } else if (n_read < 0) {
-            printf("Error %d reading from %s: %s\n", errno, portname, strerror(errno));
-            break;
-        }
-        usleep(usleep_interval); // Espera ajustada antes de intentar leer nuevamente
-    }
 
-    MPI_Finalize(); // Finalizar el entorno MPI
-    close(fd);
-    return 0;
-}
+*/
+
+
+
+
+/*
+    // Enviar comando al Arduino para ambos servos
+    snprintf(command, sizeof(command), "%s,%s\n", inputIzq, inputDer);
+    write(fd, command, strlen(command));
+*/
+
+
+
+/* Recibir el mensaje cifrado del Nodo 1 con las coordenadas para los servos
+MPI_Recv(ciphertext, 128, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+MPI_Get_count(&status, MPI_UNSIGNED_CHAR, &ciphertext_len);
+
+// Descifrar el mensaje
+decrypt(ciphertext, ciphertext_len, key, iv, decryptedtext);
+
+// Mostrar el mensaje que llega desde el nodo 1
+printf("El nodo %d envio el mensaje: %s\n", rank, decryptedtext);
+*/
+
+
+
+/*
+char str1[12];  
+char str2[12];
+
+// Convertimos los enteros a cadenas
+snprintf(str1, sizeof(str1), "%d", dx);
+snprintf(str2, sizeof(str2), "%d", dy);
+
+// Calculamos el tamaño total necesario para la cadena resultante
+int totalLength = strlen("dx") +strlen(str1) + strlen("dy") + strlen(str2) + strlen(send_data_str) + 1; // +1 para el carácter nulo
+
+// Definimos el array resultante
+char result[totalLength];
+
+// Inicializamos la cadena resultante con la primera cadena
+strcpy(result, str1);
+
+// Concatenamos la segunda cadena
+strcat(result, str2);
+
+// Concatenamos el texto adicional
+strcat(result, send_data_str);*/
